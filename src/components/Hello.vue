7a544c8840c2ff32d384fc03a78428fd1736eecc
Dennis <wynet321@163.com>
2017-09-03 16:25:22 +0800

    add element UI support

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,52 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <el-input v-model="id"/>
+    <br>
+    <el-button v-on:click="okClick" type="primary">OK</el-button>
+    <br> {{ message }}
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'hello',
+    props: {
+      message: 'aaa',
+      id: ''
+    },
+    methods: {
+      okClick () {
+        this.$http.get('http://localhost:8080/store/api/' + this.id).then((response) => {
+          this.message = response.data
+        })
+      }
     }
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
+
+  el-input {
+    maxlength: 10;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,32 +1,39 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import App from './App'
-// import router from './router'
+import App from './App'
+import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
-/* new Vue({
+new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
-}) */
-Vue.use(VueAxios, axios)
-new Vue({
-  el: '#app1',
-  data: {
-    message: 'aaa',
-    id: ''
-  },
-  methods: {
-    okClick: function () {
-      this.$http.get('http://localhost:8080/store/api/' + this.id).then((response) => {
-        this.message = response.data
-      })
-    }
-  }
+  components: { App },
+  render: h => h(App)
 })
+
+// new Vue({
+//   el: '#app1',
+//   router,
+//   data: {
+//     message: 'aaa',
+//     id: ''
+//   },
+//   render: h => h(App),
+//   methods: {
+//     okClick: function () {
+//       this.$http.get('http://localhost:8080/store/api/' + this.id).then((response) => {
+//         this.message = response.data
+//       })
+//     }
+//   }
+// })
